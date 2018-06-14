7a9e6fb8143047bf8d08100f4bf4b88e18eebf3c
Anders Jakob Sivesind <ajsivesind@gmail.com>
2018-06-14 17:11:40 +0200

    style: Improved alignment and styling

---
+++ src/app/Graph/Graph.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as styles from "./Graph.css";
+
+export class Graph extends React.Component<{},{}> {
+    render() {
+        return <div className={styles.graph} />
+    }
+}
--- src/app/Layout/Layout.tsx
+++ src/app/Layout/Layout.tsx
@@ -2,16 +2,18 @@ import * as React from 'react';
 import { Header } from 'app/Header';
 import { ChessBoard } from 'app/ChessBoard';
 import { Info } from 'app/Info';
+import { Graph } from 'app/Graph/Graph';
 
 export class Layout extends React.Component<{}, {}> {
   render() {
     return (
-      <div>
+      <div className="bg-dark" style={{minHeight: "100%", width: "100%", position: "absolute"}}>
         <Header />
         <span className="d-inline">
           <ChessBoard />
           <Info />
         </span>
+        <Graph />
       </div>
     );
   }
